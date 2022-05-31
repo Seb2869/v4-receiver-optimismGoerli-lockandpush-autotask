6d8cb8790a57af0fc412bd9f16be46ad9dea7671
Alvarez Maxence <maxence.alvarez@etu.univ-orleans.fr>
2022-05-31 20:35:50 +0200

    Creation

---
+++ README.md
@@ -0,0 +1,15 @@
+# V4 Receiver Mumbai LockAndPush Autotask
+
+Orchestrates PoolTogether V4 on Polygon Optimism Kovan Testnet
+
+**Environment Variable** 
+```.sh 
+export AUTOTASK_ID=
+```
+
+# Setup
+
+1. Copy `.envrc.example` to `.envrc`
+2. Update `.envrc` with the appropriate variables
+3. Run `direnv allow`.  See [direnv](https://direnv.net)
+4. Run `yarn update` to update the autotasks.
---
+++ src/handler.ts
@@ -0,0 +1,53 @@
+import { Relayer } from 'defender-relay-client'
+import { receiverDrawLockAndNetworkTotalSupplyPush } from '@pooltogether/v4-autotask-lib'
+import { testnet as contracts } from '@pooltogether/v4-pool-data'
+
+export async function handler(event: any) {
+  const relayer = new Relayer(event);
+  const config = {
+    beaconChain: {
+      chainId: 4,
+      providerUrl: event.secrets.ethereumRinkebyProviderURL,
+    },
+    receiverChain: {
+      chainId: 69,
+      providerUrl: event.secrets.optimismKovanProviderURL,
+    },
+    allPrizePoolNetworkChains: [
+      {
+        chainId: 4,
+        providerUrl: event.secrets.ethereumRinkebyProviderURL,
+      },
+      {
+        chainId: 80001,
+        providerUrl: event.secrets.polygonMumbaiProviderURL,
+      },
+      {
+        chainId: 43113,
+        providerUrl: event.secrets.avalancheFujiProviderURL,
+      },
+      {
+        chainId: 69,
+        providerUrl: event.secrets.optimismKovanProviderURL,
+      }
+    ]
+  }
+
+  try {
+    const transactionPopulated = await receiverDrawLockAndNetworkTotalSupplyPush(contracts, config)
+    if (transactionPopulated) {
+      let transactionSentToNetwork = await relayer.sendTransaction({
+        data: transactionPopulated.data,
+        to: transactionPopulated.to,
+        gasLimit: 500000,
+        speed: 'fast'
+      });
+      console.log('TransactionHash:', transactionSentToNetwork.hash)
+    } else {
+      throw new Error('DrawBeacon: Transaction not populated')
+    }
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
